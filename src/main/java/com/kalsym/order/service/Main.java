e7cae33e89d7805a5ba9e9a6f8088d4f46b6d0b6
shoaib-ks <shoaib@kalsym.com>
2021-02-16 16:38:03 +0500

    initial commit

---
+++ src/main/java/com/kalsym/order/service/Main.java
@@ -0,0 +1,9 @@
+package com.kalsym.order.service;
+
+/**
+ *
+ * @author 7cu
+ */
+public class Main {
+    
+}
---
+++ src/main/java/com/kalsym/order/service/controller/Controller.java
@@ -0,0 +1,9 @@
+package com.kalsym.order.service.controller;
+
+/**
+ *
+ * @author 7cu
+ */
+public class Controller {
+    
+}
