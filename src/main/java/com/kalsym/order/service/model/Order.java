5d53684661d3283ab1e866239a62e69ab1001139
taufik <taufik@kalsym.com>
2021-10-18 16:00:05 +0800

    test

--- src/main/java/com/kalsym/order/service/model/Order.java
+++ src/main/java/com/kalsym/order/service/model/Order.java
@@ -98,7 +98,7 @@ public class Order implements Serializable {
     @JoinColumn(name = "id", insertable = false, updatable = false)
     private OrderPaymentDetail orderPaymentDetail;
     
-    @OneToOne(fetch = FetchType.EAGER)
+    @ManyToOne(fetch = FetchType.EAGER)
     @JoinColumn(name = "storeId", insertable = false, updatable = false)
     private Store store;
 
