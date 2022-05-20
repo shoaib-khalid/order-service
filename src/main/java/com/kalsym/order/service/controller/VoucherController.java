92da3b9eea42d85d9e0a28be4566e370cb8dcace
taufik <taufik@kalsym.com>
2022-05-20 15:42:45 +0800

    update to 3.7.23

--- src/main/java/com/kalsym/order/service/controller/VoucherController.java
+++ src/main/java/com/kalsym/order/service/controller/VoucherController.java
@@ -211,6 +211,11 @@ public class VoucherController {
         Logger.application.info(Logger.pattern, OrderServiceApplication.VERSION, logprefix, "customerId:"+customerId+" voucherType:" + voucherType);
       
         CustomerVoucher customerVoucherMatch = new CustomerVoucher();
+        
+        if (verticalCode!=null) {
+            customerVoucherMatch.getVoucher().getVoucherVerticalList().get(0).setVerticalCode(verticalCode);
+        }
+        
         Pageable pageable = PageRequest.of(page, pageSize);
         ExampleMatcher matcher = ExampleMatcher
                 .matchingAll()
