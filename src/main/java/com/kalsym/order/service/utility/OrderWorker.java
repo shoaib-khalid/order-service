2b3788f6c32483bd8e58069aeab59e4759fd7bd1
taufik <taufik@kalsym.com>
2022-09-14 13:10:57 +0800

    bug fix

--- src/main/java/com/kalsym/order/service/utility/OrderWorker.java
+++ src/main/java/com/kalsym/order/service/utility/OrderWorker.java
@@ -220,7 +220,7 @@ public class OrderWorker {
                             return response;
                         }
                         subTotal += cartItems.get(i).getPrice() ;
-                        itemPrice = productInventory.getPrice();
+                        itemPrice = cartItems.get(i).getPrice() ;
                     }
                     
 
