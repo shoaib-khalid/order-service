a6973cece05a29b3ee516f35537d1e1fd0199bc4
taufik <taufik@kalsym.com>
2022-09-28 14:36:51 +0800

    fix payment type during place order

--- src/main/java/com/kalsym/order/service/utility/OrderWorker.java
+++ src/main/java/com/kalsym/order/service/utility/OrderWorker.java
@@ -544,8 +544,8 @@ public class OrderWorker {
                 //get order completion config
                 String verticalId = storeWithDetials.getVerticalCode();                    
                 String storeDeliveryType = storeDeliveryDetail.getType();
-                Logger.application.info(Logger.pattern, OrderServiceApplication.VERSION, logprefix, "Status:"+OrderStatus.RECEIVED_AT_STORE.name()+" VerticalId:"+verticalId+" storePickup:"+storePickup+" deliveryType:"+storeDeliveryType+" paymentType:"+storeWithDetials.getPaymentType());
-                List<OrderCompletionStatusConfig> orderCompletionStatusConfigs = orderCompletionStatusConfigRepository.findByVerticalIdAndStatusAndStorePickupAndStoreDeliveryTypeAndPaymentType(verticalId, OrderStatus.RECEIVED_AT_STORE.name(), storePickup, storeDeliveryType, storeWithDetials.getPaymentType());
+                Logger.application.info(Logger.pattern, OrderServiceApplication.VERSION, logprefix, "Status:"+OrderStatus.RECEIVED_AT_STORE.name()+" VerticalId:"+verticalId+" storePickup:"+storePickup+" deliveryType:"+storeDeliveryType+" paymentType:"+order.getPaymentType());
+                List<OrderCompletionStatusConfig> orderCompletionStatusConfigs = orderCompletionStatusConfigRepository.findByVerticalIdAndStatusAndStorePickupAndStoreDeliveryTypeAndPaymentType(verticalId, OrderStatus.RECEIVED_AT_STORE.name(), storePickup, storeDeliveryType, order.getPaymentType());
                 OrderCompletionStatusConfig orderCompletionStatusConfig = null;
                 if (orderCompletionStatusConfigs == null || orderCompletionStatusConfigs.isEmpty()) {
                     Logger.application.warn(Logger.pattern, OrderServiceApplication.VERSION, logprefix, "Status config not found for status: " + OrderStatus.RECEIVED_AT_STORE.name());             
